6a30885eed01e85e13598ca2c85710f4240cfbaf
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-05 15:36:18 +0200

    test empty for Extract

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -58,63 +58,63 @@ public class CleanExtract {
     }
     
     public static String extract(String s) {
-        var parts = s.split("\\|");
-        var result = new StringBuilder();
+        // var parts = s.split("\\|");
+        // var result = new StringBuilder();
 
-        for (var index = 0; index < parts.length; index++) {
-            var part = parts[index];
-            var partsResult = new StringBuilder();
-            var partCleaned = part.trim();
-            var pointExists = false;
+        // for (var index = 0; index < parts.length; index++) {
+        //     var part = parts[index];
+        //     var partsResult = new StringBuilder();
+        //     var partCleaned = part.trim();
+        //     var pointExists = false;
 
-            for (var i = 0; i < partCleaned.length(); i++) {
-                var c = partCleaned.charAt(i);
+        //     for (var i = 0; i < partCleaned.length(); i++) {
+        //         var c = partCleaned.charAt(i);
 
-                if (i == 0 && c == ' ') {
-                    continue;
-                }
+        //         if (i == 0 && c == ' ') {
+        //             continue;
+        //         }
                 
-                if (c == '.') {
-                    if (pointExists) {
-                        if (index == parts.length - 1) {
-                            partsResult.append(c);
-                            partsResult = silexistplusieurfoisRemover(partsResult.toString());
-                        }
-                        break;
-                    }
-                    pointExists = true;
-                    if (index == parts.length - 1 && i == partCleaned.length() - 1) {
-                        partsResult.append(c);
-                    }
-                    if (!isCharacterExists(partCleaned.substring(i))) {
-                        if (index == parts.length - 1) {
-                            partsResult.append(c);
-                        }
-                    }
-                    if (isCharacterExistsBefore(partCleaned, i)) {
-                        partsResult = new StringBuilder();
-                    }
-                    continue;
-                } 
+        //         if (c == '.') {
+        //             if (pointExists) {
+        //                 if (index == parts.length - 1) {
+        //                     partsResult.append(c);
+        //                     partsResult = silexistplusieurfoisRemover(partsResult.toString());
+        //                 }
+        //                 break;
+        //             }
+        //             pointExists = true;
+        //             if (index == parts.length - 1 && i == partCleaned.length() - 1) {
+        //                 partsResult.append(c);
+        //             }
+        //             if (!isCharacterExists(partCleaned.substring(i))) {
+        //                 if (index == parts.length - 1) {
+        //                     partsResult.append(c);
+        //                 }
+        //             }
+        //             if (isCharacterExistsBefore(partCleaned, i)) {
+        //                 partsResult = new StringBuilder();
+        //             }
+        //             continue;
+        //         } 
                 
-                if ((Character.isLetter(c) || c == ' ' || c == ',' || c == '\'' || c == '-')) { 
-                    partsResult.append(c);
-                }
-            }
+        //         if ((Character.isLetter(c) || c == ' ' || c == ',' || c == '\'' || c == '-')) { 
+        //             partsResult.append(c);
+        //         }
+        //     }
             
-            result.append(partsResult).append(" ");
-        }
+        //     result.append(partsResult).append(" ");
+        // }
         
-        if (result.length() > 0) {
-            result.setLength(result.length() - 1);
-        }
+        // if (result.length() > 0) {
+        //     result.setLength(result.length() - 1);
+        // }
 
-        if ((result.length() > 0 && result.charAt(0) == ' ') || (result.charAt(0) == '.')){
-            result.deleteCharAt(0);
-        }
+        // if ((result.length() > 0 && result.charAt(0) == ' ') || (result.charAt(0) == '.')){
+        //     result.deleteCharAt(0);
+        // }
 
-        result = spaceRemover(result.toString());
+        // result = spaceRemover(result.toString());
         
-        return result.toString();
+        // return result.toString();
     }
 }
