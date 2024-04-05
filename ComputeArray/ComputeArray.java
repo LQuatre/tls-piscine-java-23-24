afb322a67ce5fead4c9e866532aa370a894e7021
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-05 16:19:38 +0200

    ComputeArray modified

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -2,7 +2,13 @@ public class ComputeArray {
     public static int[] computeArray(int[] array) {
         var result = new int[array.length];
         for (var i = 0; i < array.length; i++) {
-            result[i] = array[i] * 2;
+            if (array[i] % 3 == 0) {
+                result[i] = array[i] * 5;
+            } else if (array[i] % 3 == 1) {
+                result[i] = array[i] + 7;
+            } else {
+                result[i] = array[i];
+            }
         }
         return result;
     }
