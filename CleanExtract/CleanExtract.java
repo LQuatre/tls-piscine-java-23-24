6bd8fc1c67c3246be576bb3eefb325947adda02f
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-05 15:28:30 +0200

    modify for '

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -93,7 +93,7 @@ public class CleanExtract {
                     continue;
                 } 
                 
-                if ((Character.isLetter(c) || c == ' ' || c == ',')) {
+                if ((Character.isLetter(c) || c == ' ' || c == ',' || c == '\'' || c == '-') && !pointExists) { 
                     partsResult.append(c);
                 }
             }
