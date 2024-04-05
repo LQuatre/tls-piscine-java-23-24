6c121dc8c158b4068a0665467365e813197ebfef
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-05 16:17:46 +0200

    we don't see anything Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,9 +1,9 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        s = s.toLowerCase();
         if (s == null) {
             return false;
         }
+        s = s.toLowerCase();
         var result = new StringBuilder();
         for (var i = s.length() - 1; i >= 0; i--) {
             result.append(s.charAt(i));
