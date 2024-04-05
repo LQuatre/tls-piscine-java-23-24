55218390c019ba09f37a5ed6a9f790676128ff69
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-05 16:14:10 +0200

    add Palindrome

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,12 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        var result = new StringBuilder();
+        for (var i = s.length() - 1; i >= 0; i--) {
+            result.append(s.charAt(i));
+        }
+        return s.equals(result.toString());
+    }
+}
