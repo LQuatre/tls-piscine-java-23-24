47e40421c701f1ca3eaea98e6936e92032c7ba66
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-12 13:52:10 +0200

    i wonder if is this true ?

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -6,9 +6,9 @@ public class SortArgs {
         Arrays.sort(numbers);
         for (int i = 0; i < numbers.length; i++) {
             if (i != numbers.length - 1) {
-            System.out.print(numbers[i] + " ");
+                System.out.print(numbers[i] + " ");
             } else {
-            System.out.print(numbers[i]);
+                System.out.println(numbers[i]);
             }
         }
     }
