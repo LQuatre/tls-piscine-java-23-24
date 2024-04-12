b5da027890262b3ff38397b56ae3b6d140b5f661
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-12 13:39:26 +0200

    modify error ^2

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -32,7 +32,7 @@ public class DoOp {
                 }
                 return Integer.toString(leftOperand % rightOperand);
             default:
-                return "Error: Invalid operator";
+                return "Error";
         }
     }
 }
