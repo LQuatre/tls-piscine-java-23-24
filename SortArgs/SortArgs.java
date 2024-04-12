c5417373841cf63044e0770d776c6ad640674879
DIOT Lucas <unmecpasfort@gmail.com>
2024-04-12 13:41:13 +0200

    add SortArgs

---
+++ SortArgs/SortArgs.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SortArgs {
+    public static void sort(String[] args) {
+        int[] numbers = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+}
